23f035ecc0f944dfc35cd95684b40829380bc612
shaokr <shaokangrun@91kook.com>
2017-03-20 14:58:55 +0800

    修改喵喵

--- demo/trunk/src/js/mobx/store.js
+++ demo/trunk/src/js/mobx/store.js
@@ -4,11 +4,11 @@
 import {useStrict} from 'mobx';
 useStrict(true);
 // 数据
-import {Title, Tab} from 'mobx/store/store';
+import {Title, Tab} from 'mobx/store';
 // 操作
-import {AppAction} from 'mobx/action/action';
+import {AppAction} from 'mobx/action';
 // 计算
-import {AppComputed} from 'mobx/computed/computed';
+import {AppComputed} from 'mobx/computed';
 
 let store = {
     title: new Title(),
