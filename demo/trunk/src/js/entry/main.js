3104c6dcd98ebffda555f414018c20fcdbba3649
shaokr <shaokangrun@91kook.com>
2017-03-21 14:56:25 +0800

    修正bug

--- demo/trunk/src/js/entry/main.js
+++ demo/trunk/src/js/entry/main.js
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom';
 
 import AppMain from 'component';
 
-import store from 'mobx/store';
+import store from 'mobx';
 import lang from 'mobx/lang';
 
 ReactDOM.render(<AppMain {...store} lang={lang} />, document.getElementById('app-main'));
