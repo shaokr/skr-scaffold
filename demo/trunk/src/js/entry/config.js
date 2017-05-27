76ece640491423d6849460c4445eedfa43bcca36
shaokr <shaokangrun@91kook.com>
2017-05-27 17:04:36 +0800

    更新config配置

--- demo/trunk/src/js/entry/config.js
+++ demo/trunk/src/js/entry/config.js
@@ -4,16 +4,11 @@
 
 import Systemjs from 'systemjs';
 import cdnHost from 'config/cdn-host';
-import _ from 'lodash';
 
-const mainData = {
-    js: `${__BUILD_PATH__}main${__BUILD_EXT__}.js`,
-    css: `${__BUILD_PATH__}main${__BUILD_EXT__}.css`
-};
+const { SystemJSConfigMain } = window;
 
 const mapListObj = { // 自定义map和依赖关系,可覆盖cdn中的配置(注释的是例子
     map: {
-        mainCss: mainData.css
         // 'ReactDom': `${cdnHost}js/react/15.4.0/react-dom.min.js`,
     },
     meta: { // map的依赖关系
@@ -25,10 +20,11 @@ const mapListObj = { // 自定义map和依赖关系,可覆盖cdn中的配置(注
 
 
 const mainListObj = { // 载入文件的配置
-    [mainData.js]: { // 入口文件
+    '_main': { // 入口文件
         ToLoad: true, // 是否马上加载
          // 依赖库
-        deps: ['mainCss', 'React', 'ReactRouter', 'mobx', 'mobxReact']
+        deps: ['mainCss', 'React', 'ReactRouter', 'mobx', 'mobxReact'
+        ].concat(SystemJSConfigMain['main'].css)
     }
 };
 
@@ -40,9 +36,10 @@ Systemjs.import(`${cdnHost}/config/1.0.4/config.js`).then((res) => {
         meta: mainListObj
     });
 
-    _.forEach(mainListObj, (item, key) => {
+    for (const key in mainListObj) {
+        const item = mainListObj[key];
         if (item.ToLoad) {
             Systemjs.import(key);
         }
-    });
+    }
 });
