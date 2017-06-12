d12011548ab2d3c0a053404bcbea72c7b1b71903
shaokr <shaokangrun@91kook.com>
2017-06-12 20:51:45 +0800

    又一次修改

--- demo/trunk/src/js/config/cdn-host.js
+++ demo/trunk/src/js/config/cdn-host.js
@@ -3,12 +3,12 @@ import env from './env';
 const { location } = window;
 // import 'common/widget/whatwg-fetch';
 // 设置公私有cdn云地址
-const _protocol = location.protocol === 'file:' ? 'http:' : '';
+const _protocol = location.protocol === 'file:' ? 'http:' : location.protocol;
 
 const host = {
-    production: `${_protocol}//192.168.1.251/fed/web-cdn/`,
-    test: `${_protocol}//192.168.1.251/fed/web-cdn/`,
-    cloud: `${location.origin}/fed/web-cdn/`
+    production: `${_protocol}//192.168.1.251/fed/web-cdn`,
+    test: `${_protocol}//192.168.1.251/fed/web-cdn`,
+    cloud: `${location.origin}/fed/web-cdn`
 }[env];
 
 export default host;
