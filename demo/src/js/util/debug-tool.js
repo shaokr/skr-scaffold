f89fbe5bee20d0b60e9a5a1e6063983f716e14f7
shaokr <shaokangrun@91kook.com>
2018-02-05 11:23:41 +0800

    优化debug-tool模块

--- demo/src/js/util/debug-tool.js
+++ demo/src/js/util/debug-tool.js
@@ -1,28 +1,31 @@
 /**
  * 开启调试: 点击5次并且按住5秒钟
  */
-import Systemjs from 'systemjs';
+// import Systemjs from 'systemjs';
 import param from 'util/param';
 import Monitor from 'util/monitor';
 
-const { debugToolOpen: windowDebugToolOpen } = window;
+const { debugToolOpen: windowDebugToolOpen, SystemJS, debugTool, Promise } = window;
 let debug = false;
 let monitorList = new Monitor();
+const cachingTime = 1000 * 60 * 2;// // 设置清除缓存时间
 // 开启
 const debugToolOpen = () => {
     debug = true;
-    monitorList.go(Systemjs.import('debug-tool'));
+    if (debugTool) {
+        monitorList.go(Promise.resolve(debugTool));
+    } else if (SystemJS) {
+        monitorList.go(SystemJS.import('debug-tool'));
+    }
 };
-
 if (param.debug) {
     debug = true;
 } else {
-    // 2分钟设置清除缓存
     setTimeout(() => {
         if (!debug) {
             monitorList = null;
         }
-    }, 1000 * 60 * 2);
+    }, cachingTime);
 }
 let i = 0;
 let ct = 0;
@@ -41,8 +44,7 @@ const onOpenDebug = () => {
         }, 500);
     } else {
         ct = setTimeout(() => {
-            debug = true;
-            monitorList.go(Systemjs.import('debug-tool'));
+            debugToolOpen();
             document.removeEventListener('click', onOpenDebug);
             document.removeEventListener('click', onClearTimeout);
         }, 5000);
@@ -52,13 +54,15 @@ document.addEventListener('click', onOpenDebug);
 document.addEventListener('click', onClearTimeout);
 
 function tool(key, ...data) {
-    monitorList.once((debugToolPromise) => {
-        debugToolPromise.then((Tool) => {
-            Tool(key, ...data);
+    if (monitorList) {
+        monitorList.once((debugToolPromise) => {
+            debugToolPromise.then((Tool) => {
+                Tool(key, ...data);
+            });
         });
-    });
-    if (debug) {
-        monitorList.go(Systemjs.import('debug-tool'));
+        if (debug) {
+            debugToolOpen();
+        }
     }
 }
 
