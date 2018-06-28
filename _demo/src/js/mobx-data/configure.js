f4fab9b79b1ba5ab9b18996f8dd4a0fccd15d99b
shaokr <shaokangrun@91kook.com>
2018-06-28 15:42:02 +0800

    修改_demo中的mobx

---
+++ _demo/src/js/mobx-data/configure.js
@@ -0,0 +1,6 @@
+/**
+ * mobx配置
+ */
+import { configure } from 'mobx';
+
+configure({ enforceActions: true });
--- _demo/src/js/mobx-data/index.js
+++ _demo/src/js/mobx-data/index.js
@@ -1,7 +1,7 @@
 /**
  * 数据,操作,计算 整合文件
  */
-import { useStrict } from 'mobx';
+import './configure';
 // 数据
 import { Title, Tab } from './store';
 // 操作
@@ -9,23 +9,17 @@ import { AppAction } from './action';
 // 计算
 import { AppComputed } from './computed';
 
-useStrict(true);
+export default new class {
+  store = {
+    title: new Title(),
+    tab: new Tab()
+  };
 
-const store = {
-  title: new Title(),
-  tab: new Tab()
-};
+  action = {
+    title: new AppAction(this.store)
+  };
 
-const action = {
-  title: new AppAction(store)
-};
-
-const computed = {
-  title: new AppComputed(store)
-};
-
-export default {
-  store,
-  action,
-  computed
-};
+  computed = {
+    title: new AppComputed(this)
+  };
+}();
