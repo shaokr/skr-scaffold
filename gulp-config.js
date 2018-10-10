6a899bfbdf977336fbd479ba8c2fcddfa73f28d7
shaokr <shaokangrun@91kook.com>
2018-10-10 14:44:17 +0800

    格式代码

--- gulp-config.js
+++ gulp-config.js
@@ -1,20 +1,21 @@
 let path = require('path');
 module.exports = {
-    path: path.resolve(__dirname, 'project'), // 项目目录
-    webpackConfig: path.resolve(__dirname, 'webpack.production.js'), // 共用webpack配置
-    browsers: ['> 1%', 'last 5 versions'],
-    modules: ['js', 'less', 'img'], // 查找资源的目录相对于 src中的path地址
-    src: {
-        path: 'src', // 目录地址(与src同级
-        js: 'js/entry', // 需要编译的js目录
-        css: 'less/pages', // 需要编译的样式目录
-        html: 'pages', // 需要编译的html目录
-        packconf: ['packconf/config.js', '../packconf/config.js'] // 项目独立webpack配置
-    },    // 源码目录名
-    dist: { // 产出配置
-        path: 'dist', // 目录地址(与src同级
-        // build: 'build', // htmljs和css目录
-        // html: '.',
-        img: 'img' // img目录
-    }
+  path: path.resolve(__dirname, 'project'), // 项目目录
+  webpackConfig: path.resolve(__dirname, 'webpack.production.js'), // 共用webpack配置
+  browsers: ['> 1%', 'last 5 versions'],
+  modules: ['js', 'less', 'img'], // 查找资源的目录相对于 src中的path地址
+  src: {
+    path: 'src', // 目录地址(与src同级
+    js: 'js/entry', // 需要编译的js目录
+    css: 'less/pages', // 需要编译的样式目录
+    html: 'pages', // 需要编译的html目录
+    packconf: ['packconf/config.js', '../packconf/config.js'] // 项目独立webpack配置
+  }, // 源码目录名
+  dist: {
+    // 产出配置
+    path: 'dist', // 目录地址(与src同级
+    // build: 'build', // htmljs和css目录
+    // html: '.',
+    img: 'img' // img目录
+  }
 };
